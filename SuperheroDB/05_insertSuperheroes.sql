87a99015c6d71bf7e5a61a928ae48ac4ea320101
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-02-17 11:09:29 +0200

    FEAT: Add SQL script for inserting three superheroes

---
+++ SuperheroDB/05_insertSuperheroes.sql
@@ -0,0 +1,10 @@
+USE SuperheroesDb;
+
+INSERT INTO Superhero (Name, Alias, Origin)
+	VALUES ('Bruce Wayne', 'Batman', 'Gotham City');
+
+INSERT INTO Superhero (Name, Alias, Origin)
+	VALUES ('Clark Kent', 'Superman', 'Metropolis');
+
+INSERT INTO Superhero (Name, Alias, Origin)
+	VALUES ('Bruce Banner', 'Hulk', 'Dayton');
