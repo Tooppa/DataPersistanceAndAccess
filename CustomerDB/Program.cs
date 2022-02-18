03abfb19961a0852ada1f77e727115146b7a5251
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-02-18 09:25:10 +0200

    FEAT: Add method for selecting customer from the database by id

--- CustomerDB/Program.cs
+++ CustomerDB/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace CustomerDB 
+﻿using CustomerDB.Models;
+using CustomerDB.Repositories;
+
+namespace CustomerDB 
 {
     public class Program
     {
         public static void Main()
         {
+            ICustomerRepository repository = new CustomerRepository();
+            //TestSelectById(repository);
+        }
 
+        static void TestSelectById(ICustomerRepository repository)
+        {
+            Customer customer = repository.GetById(1);
+            Console.WriteLine(customer.ToString());
         }
     }
 }
