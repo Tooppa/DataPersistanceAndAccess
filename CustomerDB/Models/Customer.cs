9ef7908d6d9cd5b2c96c20a85fa256caaeae345a
Timo Järvenpää <tj.jarvenpaa@gmail.com>
2022-02-18 08:35:35 +0200

    FEAT: Add fields to Customer class

--- CustomerDB/Models/Customer.cs
+++ CustomerDB/Models/Customer.cs
@@ -2,5 +2,17 @@
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public override string? ToString()
+        {
+            return this.CustomerId + " - " + this.FirstName + " " + this.LastName + " - " + this.Country;
+        }
     }
 }
